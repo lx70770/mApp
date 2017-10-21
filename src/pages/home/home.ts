245118d2539a2dca2f827306f3ef192161b63a41
lx70770 <lx70770@gmail.com>
2017-10-21 22:21:59 +0800

    第一次提交

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, OnInit} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Http} from "@angular/http";
+import "rxjs/Rx";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  dataSource: Observable<any>;
 
+  notes = [];
+
+  constructor(public navCtrl: NavController,public http: Http) {
+    this.dataSource = this.http.get('http://localhost:8080/stock').map(response => response.json());
   }
 
+  ngOnInit() {
+    this.dataSource.subscribe(
+      data => this.notes = data
+    )
+  }
 }
+
